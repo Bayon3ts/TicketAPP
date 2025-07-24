6502500d68faa412817a17857fe818049cb313d6
Bayonet <bayomisimon@gmail.com>
2025-07-24 05:22:34 +0100

    your message

---
+++ lib/base/bottom_Nav_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
